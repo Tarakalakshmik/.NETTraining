fd30869cff7a9ebfa7af3b9f48da799ce9db79ee
Taraka Lakshmi <tarakal@infinite.com>
2025-06-18 07:13:32 +0530

    (assignmnetday1)

--- Assignments/Assignement1/Assignement1/Program.cs
+++ Assignments/Assignement1/Assignement1/Program.cs
@@ -4,23 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Assignment1
+namespace Assignement1
 {
-    class question
+    class question1
     {
         public static void Main()
         {
-            question.Are_equal();
+            question1.Are_equal();
+
         }
-         public static void Are_equal()
+        public static void Are_equal()
         {
-            int num1,num2;
+            int num1, num2;
             Console.WriteLine("Enter the first number");
             num1 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Enter the second number");
             num2 = Convert.ToInt32(Console.ReadLine());
-            if(num1==num2)
-                Console.WriteLine("Num1{0} and Num2{1} are equal",num1,num2);
+            if (num1 == num2)
+                Console.WriteLine("Num1{0} and Num2{1} are equal", num1, num2);
             else
                 Console.WriteLine("Num1{0} and Num2{1} are not equal", num1, num2);
             Console.ReadKey();
