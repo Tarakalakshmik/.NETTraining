e6307dbe7d0bd5badabf09d5a98c981be7917789
Taraka Lakshmi <tarakal@infinite.com>
2025-06-25 00:36:09 +0530

    (message)

---
+++ CSHARP/Assignment/Assignmentday3/Assignment3/Assignment3/Question1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3
+{
+    public class Accounts
+    {
+
+        public int acc_no;
+        public string name;
+        public string acc_type;
+        public char trac_tp;
+        public Double amount;
+        public Double balance;
+
+        public Accounts(int acc_no, string name, string acc_type, double bal)
+        {
+            this.acc_no = acc_no;
+            this.name = name;
+            this.acc_type = acc_type;
+            balance = bal;
+
+
+        }
+        public void credit(double balance, double amount)
+        {
+            balance = amount + balance;
+        }
+        public void debit(double balance, double amount)
+        {
+            balance = balance - amount;
+        }
+        public void show_data()
+        {
+            Console.WriteLine($"Account number is {acc_no}");
+            Console.WriteLine($"Name is {name}");
+            Console.WriteLine($"Account type is {acc_type}");
+            Console.WriteLine($"Transcation type  {trac_tp}");
+            Console.WriteLine($"Balance is {balance}");
+        }
+    }
+        class Question1 
+        {
+            static void Main(string[] args)
+            {
+                Accounts b = new Accounts(123,"abc","Savings",1000);
+            
+                Console.WriteLine("Enter transaction type as D(deposit) or W(withdrawal");
+                
+                  b.trac_tp = Convert.ToChar(Console.ReadLine());
+
+                //b.amount = Convert.ToDouble(Console.ReadLine());
+                
+                if(b.trac_tp=='D')
+                {
+                    Console.WriteLine("Enter amount");
+                    b.credit(b.balance,Convert.ToDouble(Console.ReadLine()));
+                    b.show_data();
+                }
+               
+                if(b.trac_tp=='W')
+                {
+                    Console.WriteLine("Enter amount");
+                    b.debit(b.balance,Convert.ToDouble(Console.ReadLine()));
+                    b.show_data();
+                }
+               
+                Console.Read();
+
+            }
+        }
+    }
+
+
---
+++ CSHARP/Assignment/Assignmentday3/Assignment3/Assignment3/Question2.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3
+{
+    public class Student
+    {
+        public int rollno;
+        public string name;
+        public string sem;
+        public string branch;
+        public int[] marks = new int[5];
+        public void GetMarks()
+        {
+            for(int i=0;i<5;i++)
+            {
+                Console.WriteLine("Enter subject {0} marks",i+1);
+                marks[i] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+        public Student(int rollno,string name,string sem,string branch)
+        {
+            this.rollno = rollno;
+            this.name = name;
+            this.sem = sem;
+            this.branch = branch;
+        }
+        public void displaydetails()
+        {
+            Console.WriteLine("Rollno is {0}",rollno);
+            Console.WriteLine("Rollno is {0}", name);
+            Console.WriteLine("Rollno is {0}", sem);
+            Console.WriteLine("Rollno is {0}", branch);
+        }
+        public void displayresult(int[] marks) 
+        {
+            double avg = marks.Average();
+            Console.WriteLine("{0}",avg);
+            bool flag = true;
+            for (int i = 0; i < 5; i++)
+            {
+                if (marks[i] < 35)
+                {
+                    flag = false;
+                    Console.WriteLine("Failed");
+                    return;
+
+                }
+            }
+                    if(avg>50 & flag)
+                    {
+                        Console.WriteLine("Passed");
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Failed");
+                    }
+                
+
+                
+            }
+        }
+
+    class Question2
+    {
+        
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("Enter Roll Number");
+            int rollno = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Name");
+            string name = Console.ReadLine();
+            Console.WriteLine("Enter Semester");
+            string sem = Console.ReadLine();
+            Console.WriteLine("Enter Branch");
+            string branch = Console.ReadLine();
+            Student s = new Student(rollno, name, sem, branch);
+            s.GetMarks();
+            s.displayresult(s.marks);
+            Console.Read();
+
+        }
+
+    }
+
+}
