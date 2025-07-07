abfa1207c9cea3aca5c21c98e4ae64545934d764
Taraka Lakshmi <tarakal@infinite.com>
2025-07-07 22:00:19 +0530

    message

---
+++ CSHARP/Assignment/Assignment6/Assignment6/Question3.cs
@@ -0,0 +1,42 @@
+﻿//3. Write a program in C# Sharp to count the number of lines in a file.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace Assignment6
+{
+    class Question3
+    {
+        static void Main()
+        {
+            string filepath = "C:\\Users\\tarakal\\Documents\\.NetTraining\\CSHARP\\Assignment\\Ass6q4.txt";
+            //string fp = "C:\\Users\\tarakal\\Documents\\.NetTraining\\CSHARP\\Assignment\\Ass6q3.txt";
+
+            try
+            {
+                string[] lines = File.ReadAllLines(filepath);
+                //string[] l = File.ReadAllLines(fp);
+                int count = lines.Length;
+                //int c = l.Length;
+                Console.WriteLine($"Number of lines in the file {filepath} are: {count}");
+                //Console.WriteLine($"Number of lines in the file {fp} are: {c}");
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("Error: File not present at the given path");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error in reading the file", e.Message);
+            }
+
+            Console.Read();
+        }
+    }
+}
+
+
+
---
+++ CSHARP/Assignment/Assignment7/Travel_concess_lib/TravelConcess.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Travel_concess_lib
+{
+    public class TravelConcess
+    {
+        public void CalculateConcession(string name, int age, double totalFare)
+        {
+            if (age <= 5)
+            {
+                Console.WriteLine($"{name}: Little Champ - Free Ticket");
+            }
+            else if (age > 60)
+            {
+                double discountedFare = totalFare - (totalFare * 0.30); // 30% discount
+                Console.WriteLine($"{name}: Senior Citizen - Fare after 30% concession: {discountedFare}");
+            }
+            else
+            {
+                Console.WriteLine($"{name}: Ticket Booked - Fare: {totalFare}");
+            }
+        }
+    }
+}
