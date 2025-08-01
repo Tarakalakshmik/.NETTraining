7b886adf4a3fb802f0c4262a32ac7a0eed2602ba
Taraka Lakshmi <tarakal@infinite.com>
2025-08-01 22:40:56 +0530

    assign2

---
+++ Web Technologies/Practice/JSPROGRAMS/JS/JS/JavaScript1.js
@@ -0,0 +1,14 @@
+﻿
+calle();
+function calle() {
+    var firstname = "Taraka"
+    var lastname="Lakshmi"
+    const fullName = called(firstname, lastname);
+    console.log("Full Name:", fullName);
+}
+
+function called(firstname, lastname) {
+    return firstname + " " + lastname;
+}
+
+
