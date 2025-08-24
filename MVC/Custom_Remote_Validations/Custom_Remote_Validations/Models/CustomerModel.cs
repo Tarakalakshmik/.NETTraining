48a25f44ee67abf040884702e5aa1273ffc76d73
Taraka Lakshmi <tarakal@infinite.com>
2025-08-24 10:12:36 +0530

    assignment

---
+++ MVC/Custom_Remote_Validations/Custom_Remote_Validations/Models/CustomerModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
+namespace Custom_Remote_Validations.Models
+{
+    public class CustomerModel
+    {
+        public string Name { get; set; }
+        [Remote("IsEmailExists","Customer",ErrorMessage="Email already exists")]
+        public string Email { get; set; }
+    }
+}
---
+++ MVC/Custom_Remote_Validations/Custom_Remote_Validations/Models/UserModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+namespace Custom_Remote_Validations.Models
+{
+    public class UserModel:IValidatableObject
+    {
+        public DateTime BirthDay { get; set; }
+        public string Name { get; set; }
+        public IEnumerable<ValidationResult>Validate(ValidationContext validationContext)
+        {
+            if (this.BirthDay.Year < 1996)
+                yield return new ValidationResult("Seems you are a bit Old for this job", new[] { "BirthDay" });
+            if (this.BirthDay.Year > 2003)
+                yield return new ValidationResult("Seems you are a young for this job", new[] { "BirthDay" });
+            if (this.BirthDay.Month == 4)
+                yield return new ValidationResult("Sorry, we dont accept april borns", new[] { "BirthDay" });
+            if ((this.BirthDay.DayOfWeek==DayOfWeek.Wednesday) && (this.Name!="Shivaji"))
+                    yield return new ValidationResult("Sorry to have been born on a wednesday,and we need your name to be Shivaji", new[] { "BirthDay", "Name" });
+        }
+    }
+}
